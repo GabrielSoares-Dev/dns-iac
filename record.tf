8b75b4b7f78fa56a9179f502ac756cfe0521eba5
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-31 21:40:58 -0300

    feat: configure domain

--- record.tf
+++ record.tf
@@ -1,6 +1,5 @@
-resource "aws_route53_record" "example_subdomain" {
-  for_each = var.example_subdomains
-  zone_id  = aws_route53_zone.primary.id
-  name     = each.value.name
-  type     = "A"
-}
+# resource "aws_route53_record" "example_subdomain" {
+#   for_each = var.example_subdomains
+#   zone_id  = aws_route53_zone.primary.id
+#   name     = each.value.name
+# }
