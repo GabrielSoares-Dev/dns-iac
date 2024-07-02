f5cc8acf6d98b213f027d83816d75b702da33fdf
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-07-02 14:09:03 -0300

    feat: add api gateway domain

--- api-gw-domain-ssm.tf
+++ api-gw-domain-ssm.tf
@@ -1,4 +1,4 @@
-# data "aws_ssm_parameter" "zone_id_api_gateway_domain_name_dev" {
+# data "aws_ssm_parameter" "zone_id_api_gw_domain_name_dev" {
 #   name = "/dev/api-gw/boilerplate-laravel-10-serverless/domain/name"
 # }
 
@@ -6,10 +6,27 @@
 #   name = "/dev/api-gw/boilerplate-laravel-10-serverless/domain/zone-id"
 # }
 
-# data "aws_ssm_parameter" "zone_id_api_gateway_domain_name_dev" {
+# data "aws_ssm_parameter" "zone_id_api_gw_domain_name_dev" {
 #   name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/name"
 # }
 
 # data "aws_ssm_parameter" "api_gw_nestjs_boilerplate_zone_id_dev" {
 #   name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
 # }
+
+data "aws_ssm_parameter" "zone_id_api_gw_domain_nestjs_name_dev" {
+  name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/name"
+}
+
+data "aws_ssm_parameter" "api_gw_nestjs_boilerplate_zone_id_dev" {
+  name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
+}
+
+data "aws_ssm_parameter" "zone_id_api_gateway_domain_name_dev" {
+  name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/name"
+}
+
+data "aws_ssm_parameter" "api_gw_nestjs_boilerplate_zone_id_dev" {
+  name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
+}
+
--- record.tf
+++ record.tf
@@ -1,15 +1,15 @@
-# resource "aws_route53_record" "example_subdomain" {
-#   for_each = var.example_subdomains
-#   zone_id  = aws_route53_zone.primary.id
-#   name     = each.value.name
-#   type     = "A"
+resource "aws_route53_record" "example_subdomain" {
+  for_each = var.example_subdomains
+  zone_id  = aws_route53_zone.primary.id
+  name     = each.value.name
+  type     = "A"
 
-#   alias {
-#     name                   = data.aws_ssm_parameter.zone_id_api_gateway_domain_name_dev.value
-#     zone_id                = data.aws_ssm_parameter.api_gw_nestjs_boilerplate_zone_id_dev.value
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = data.aws_ssm_parameter.zone_id_api_gateway_domain_name_dev.value
+    zone_id                = data.aws_ssm_parameter.api_gw_nestjs_boilerplate_zone_id_dev.value
+    evaluate_target_health = false
+  }
+}
 
 
 # resource "aws_route53_record" "db_mysql_subdomain" {
