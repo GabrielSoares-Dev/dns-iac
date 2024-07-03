c6dc8993fd0144e87dc58bc5c61d2bfa8e131eb3
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-07-03 09:35:15 -0300

    uncomment files

--- api-gw-domain-ssm.tf
+++ api-gw-domain-ssm.tf
@@ -10,20 +10,16 @@
 #   name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/name"
 # }
 
-# data "aws_ssm_parameter" "api_gw_nestjs_boilerplate_zone_id_dev" {
-#   name = "/dev/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
-# }
+data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_id" {
+  name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/id"
+}
 
-# data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_id" {
-#   name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/id"
-# }
+data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_domain_name" {
+  name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/domain/name"
+}
 
-# data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_domain_name" {
-#   name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/domain/name"
-# }
-
-# data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_domain_zone_id" {
-#   name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
-# }
+data "aws_ssm_parameter" "boilerplate_nestjs_serverless_ssm_api_gw_domain_zone_id" {
+  name = "/${lower(var.environment)}/api-gw/boilerplate-nestjs-serverless/domain/zone-id"
+}
 
 
